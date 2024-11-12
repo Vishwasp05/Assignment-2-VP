963d96cc20c51b1d3786619abc12160b6415306f
sharma-vishwas <vishwasp@protonmail.com>
2024-11-12 18:32:19 +0530

    Initial Commit

---
+++ Assignment-2-VP/Assignment_2_VPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_2_VPApp.swift
+//  Assignment-2-VP
+//
+//  Created by Vishwas Sharma on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_2_VPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment-2-VP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment-2-VP
+//
+//  Created by Vishwas Sharma on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
